f4ea334824e24c02a01b733c5b7439758393369d
曹宝琪 <2023cbq@gmail.com>
2023-11-22 14:06:27 +0800

    plugins: show comment

---
+++ plugins-init/src/main/java/jz/cbq/githubCopilot/Quickstart.java
@@ -0,0 +1,26 @@
+package jz.cbq.githubCopilot;
+
+/**
+ * GithubCopilot 快速入门
+ *
+ * @author cbq
+ * @date 2023/11/22 13:37
+ * @since 1.0.0
+ */
+public class Quickstart {
+
+    public static void main(String[] args) {
+        overview();
+    }
+
+    /**
+     * githubCopilot 是一款 IntelliJ IDEA 插件,用于代码自动补全
+     * 其内置 GPT-3.5 语言模型,可以根据上下文自动补全代码
+     * 功能如下：
+     * 1. 代码补全
+     * 2. chat 聊天
+     */
+    private static void overview(){
+        System.out.println("Hello GithubCopilot");
+    }
+}
---
+++ plugins-init/src/main/java/jz/cbq/showComment/Quickstart.java
@@ -0,0 +1,28 @@
+package jz.cbq.showComment;
+
+/**
+ * showComment 快速入门
+ *
+ * @author cbq
+ * @date 2023/11/22 13:55
+ * @since 1.0.0
+ */
+public class Quickstart {
+
+    public static void main(String[] args) {
+        overview();
+    }
+
+
+    /**
+     * showComment 是一款 IntelliJ IDEA 插件，可以在代码中显示注释
+     * 功能如下：
+     *   在方法上方显示注释 (showComment 快速入门)
+     * ? 高亮注释 `?` 表示不确定
+     * ! 高亮注释 `!` 表示警告
+     * * 高亮注释 `*` 表示提示
+     */
+    private static void overview(){
+        System.out.println("Hello showComment");
+    }
+}
