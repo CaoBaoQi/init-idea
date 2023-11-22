99677ef0c5dbe96df902961da87b27ce5ddc8c9a
曹宝琪 <2023cbq@gmail.com>
2023-11-22 14:16:35 +0800

    plugins-better highlights quickstart

---
+++ plugins-init/src/main/java/jz/cbq/betterHighlights/Quickstart.java
@@ -0,0 +1,26 @@
+package jz.cbq.betterHighlights;
+
+/**
+ * Quickstart
+ *
+ * @author cbq
+ * @date 2023/11/22 14:13
+ * @since 1.0.0
+ */
+public class Quickstart {
+    public static void main(String[] args) {
+        overview();
+    }
+
+
+    /**
+     * betterHighlights 是一款 IntelliJ IDEA 插件，可以在代码中显示注释
+     * 功能如下：
+     * ? 高亮注释 `?` 表示不确定
+     * ! 高亮注释 `!` 表示警告
+     * * 高亮注释 `*` 表示提示
+     */
+    private static void overview(){
+        System.out.println("Hello better highlights");
+    }
+}
